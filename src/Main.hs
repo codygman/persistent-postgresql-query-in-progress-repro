6a6c35a49420dc5606f83ae299d2cb29e0f7196c
codygman <cody@codygman.dev>
2021-03-09 11:33:10 -0600

    make postgresql simple example closer to persistent

--- src/Main.hs
+++ src/Main.hs
@@ -4,6 +4,7 @@
 {-# OPTIONS_GHC -Wno-deferred-type-errors #-}
 module Main where
 
+import Data.Acquire (Acquire, ReleaseType(..), mkAcquireType, with, allocateAcquire)
 import  qualified Control.Monad as Monad
 import qualified Control.Concurrent as Concurrent
 import qualified Control.Exception as Exception
@@ -15,6 +16,9 @@ import qualified Data.ByteString as BS
 import qualified Data.Pool as Pool
 import Data.Time
 import qualified Database.PostgreSQL.Simple as PGS
+import Debug.Trace
+import Control.Monad.Trans.Resource
+import Control.Monad.IO.Class
 
 main :: IO ()
 main = do
@@ -29,12 +33,25 @@ main = do
     PGS.execute_ conn "create table if not exists foo(id int);"
 
 
+
+  let freeConn (res, localPool) relType =
+        case (("relType: " <> show relType) `trace` relType) of
+          ReleaseException -> Pool.destroyResource pool localPool res
+          _ -> Pool.putResource localPool res
+
+  let acq = fst <$> mkAcquireType (Pool.takeResource pool) freeConn
+
   threadId <- Concurrent.forkIO $ do
-      Pool.withResource pool $ \conn -> do
-        let numThings :: Int = 100000000
-        putStrLn $ "start inserting " <> show numThings <> " things"
-        Monad.forM_ [1 .. numThings] $ \_ ->  do
-          PGS.execute_ conn "insert into foo values(1);"
+
+      -- TODO simulate what persistent does
+      -- https://github.com/codygman/persistent/blob/3d34fed5f2958c1e2071d2846ec0dd785dacb69d/persistent/Database/Persist/Sql/Run.hs#L46
+
+    Monad.void . with acq $ \conn -> do
+      liftIO $ PGS.execute_ conn "insert into foo values(1);"
+      let numThings :: Int = 100000000
+      putStrLn $ "start inserting " <> show numThings <> " things"
+      Monad.forM_ [1 .. numThings] $ \_ ->  do
+        PGS.execute_ conn "insert into foo values(1);"
 
 
   putStrLn "waiting for insert thread to make progress"
@@ -42,7 +59,7 @@ main = do
   Monad.void $ Concurrent.killThread threadId
   putStrLn "killing insert thread"
 
-  Pool.withResource pool $ \conn -> do
-    PGS.execute_ conn "insert into foo values(1);"
+  Monad.void . with acq $ \conn -> do
+      liftIO $ PGS.execute_ conn "insert into foo values(1);"
 
   putStrLn "done"
